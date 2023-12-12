a9134a5ff6351ce62ca693835b65b42422c3016d
zomby-fraser <afraser1@stevens.edu>
2023-12-12 00:23:30 -0500

    test

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,12 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def loginPage():
+    # return "Hello"
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
