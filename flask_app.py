865f188e71104ae8e920c463ef397069465532b1
zomby-fraser <afraser1@stevens.edu>
2023-12-12 06:25:04 -0500

    add logins

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import hashlib
+import mysql.connector
 
 app = Flask(__name__)
 
 @app.route('/')
 def loginPage():
-    # return "Hello"
     return render_template('login.html')
+
+@app.route('/home')
+def homePage():
+    return "Hello"
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not username or not password:
+        return jsonify({'error': 'Missing username or password'}), 400
+
+    # Hash the password
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+    try:
+        # Connect to the database
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+
+        # Query to find the user
+        query = "SELECT * FROM users WHERE username = %s AND password = %s"
+        cursor.execute(query, (username, hashed_password))
+
+        user = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if user:
+            return jsonify({'message': 'Login successful'}), 200
+        else:
+            return jsonify({'error': 'Invalid credentials'}), 401
+
+    except mysql.connector.Error as err:
+        return jsonify({'error': str(err)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
