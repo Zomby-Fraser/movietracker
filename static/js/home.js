4e133b9ce1d34ebde896ca68b833ddf155e009a9
zomby-fraser <afraser1@stevens.edu>
2023-12-13 00:44:45 -0500

    add logic to btn

--- static/js/home.js
+++ static/js/home.js
@@ -12,13 +12,33 @@ document.getElementById('imdbQueryRadio').addEventListener('change', function() 
     document.getElementById('searchFields').style.display = 'block';
 });
 
-function submitMovieRequest() {
-    var title = document.getElementById('movieTitle').value;
-    var year = document.getElementById('movieYear').value;
-    var imdbLink = document.getElementById('imdbLink').value;
+async function submitMovieRequest() {
+    if (document.getElementById('requestMovieBtn').checked) {
+        var title = document.getElementById('movieTitle').value;
+        var year = document.getElementById('movieYear').value;
+        var post_body = `title=${title}&year=${year}`;
+    }
+    else {
+        var imdb_link = document.getElementById('imdbLink').value;
+        var post_body = `url=${imdb_link}`;
+    }
+    
 
     // Logic to handle the movie request submission
     // This might involve an AJAX request to your Flask backend
+    const response = await fetch('http://zombyfraser.pythonanywhere.com/add_movie', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: post_body
+    });
+    if (response.ok) {
+        const data = await response.json();
+        console.log('Scrape Successful:', data);
+    } else {
+        console.error('Login failed:', response.status, response.statusText);
+    }
 
     closeModal(); // Close the modal after submission
 }
